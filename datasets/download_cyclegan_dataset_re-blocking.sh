2c361b58d7f13f8183df042f213d2df2d68ec7df
Demircan Tas <tasd@mit.edu>
2023-11-06 18:09:04 -0500

    revert to no if statement

--- datasets/download_cyclegan_dataset_re-blocking.sh
+++ datasets/download_cyclegan_dataset_re-blocking.sh
@@ -6,10 +6,15 @@ if [[ $FILE != "ae_photos" && $FILE != "manhattan" ]]; then
     exit 1
 fi
 
+# if [[ $FILE == "manhattan" ]]; then
+#     URL="https://www.dropbox.com/scl/fi/rv4p9brw0ouq0a8kcdukh/manhattan.zip?rlkey=jknrhqrezfp7gj6sm78svnv6i&dl=1"
+# fi
+
 echo "Specified [$FILE]"
 ZIP_FILE=./datasets/$FILE.zip
 TARGET_DIR=./datasets/$FILE/
 URL="https://www.dropbox.com/scl/fi/rv4p9brw0ouq0a8kcdukh/manhattan.zip?rlkey=jknrhqrezfp7gj6sm78svnv6i&dl=1"
+# URL="_"
 wget -N $URL -O $ZIP_FILE
 mkdir $TARGET_DIR
 unzip $ZIP_FILE -d ./datasets/
@@ -17,7 +22,3 @@ rm $ZIP_FILE
 # Demircan's modification
 # From: https://dl.dropboxusercontent.com/scl/fi/rv4p9brw0ouq0a8kcdukh/manhattan.zip?rlkey=jknrhqrezfp7gj6sm78svnv6i&dl=0
 # replace 'www.dropbox.com' with 'dl.dropboxusercontent.com'
-
-# if [[ $FILE == "manhattan" ]]; then
-#     URL=https://dl.dropboxusercontent.com/scl/fi/rv4p9brw0ouq0a8kcdukh/manhattan.zip?rlkey=jknrhqrezfp7gj6sm78svnv6i&dl=0
-# fi

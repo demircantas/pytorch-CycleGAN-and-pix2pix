f3fb8a9cb7ca0fc679b3f1ca5e75690b18c99520
Demircan Tas <tasd@mit.edu>
2023-11-06 18:05:05 -0500

    fixed link from Dropbox

--- datasets/download_cyclegan_dataset_re-blocking.sh
+++ datasets/download_cyclegan_dataset_re-blocking.sh
@@ -6,21 +6,18 @@ if [[ $FILE != "ae_photos" && $FILE != "manhattan" ]]; then
     exit 1
 fi
 
-# ---
-# Demircan's modification
-# From: https://dl.dropboxusercontent.com/scl/fi/rv4p9brw0ouq0a8kcdukh/manhattan.zip?rlkey=jknrhqrezfp7gj6sm78svnv6i&dl=0
-# replace 'www.dropbox.com' with 'dl.dropboxusercontent.com'
-
 echo "Specified [$FILE]"
-
-URL=https://dl.dropboxusercontent.com/scl/fi/rv4p9brw0ouq0a8kcdukh/manhattan.zip?rlkey=jknrhqrezfp7gj6sm78svnv6i&dl=0
-# if [[ $FILE == "manhattan" ]]; then
-#     URL=https://dl.dropboxusercontent.com/scl/fi/rv4p9brw0ouq0a8kcdukh/manhattan.zip?rlkey=jknrhqrezfp7gj6sm78svnv6i&dl=0
-# fi
-
 ZIP_FILE=./datasets/$FILE.zip
 TARGET_DIR=./datasets/$FILE/
+URL="https://www.dropbox.com/scl/fi/rv4p9brw0ouq0a8kcdukh/manhattan.zip?rlkey=jknrhqrezfp7gj6sm78svnv6i&dl=1"
 wget -N $URL -O $ZIP_FILE
 mkdir $TARGET_DIR
 unzip $ZIP_FILE -d ./datasets/
 rm $ZIP_FILE
+# Demircan's modification
+# From: https://dl.dropboxusercontent.com/scl/fi/rv4p9brw0ouq0a8kcdukh/manhattan.zip?rlkey=jknrhqrezfp7gj6sm78svnv6i&dl=0
+# replace 'www.dropbox.com' with 'dl.dropboxusercontent.com'
+
+# if [[ $FILE == "manhattan" ]]; then
+#     URL=https://dl.dropboxusercontent.com/scl/fi/rv4p9brw0ouq0a8kcdukh/manhattan.zip?rlkey=jknrhqrezfp7gj6sm78svnv6i&dl=0
+# fi
